cf2504a4fadf3be207c321908a9127157d9c9d64
rim <devsjrim@gmail.com>
2025-11-05 18:17:32 +0900

    init

---
+++ src/main/java/com/devsjrim/simple_order_service/SimpleOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devsjrim.simple_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleOrderServiceApplication.class, args);
+    }
+
+}
